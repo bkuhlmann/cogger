1e34ec0f3e1c26111a5ee1c0f438ddea678c6cf4
Brooke Kuhlmann <brooke@alchemists.io>
2022-04-02 11:03:01 -0600

    Added client
    Necessary to decorate an existing logger with colorized output using
    default colors for all log levels.

---
+++ lib/cogger/client.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "forwardable"
+require "logger"
+
+module Cogger
+  # Provides the primary client for colorized logging.
+  class Client
+    extend Forwardable
+
+    using Refinements::Loggers
+
+    delegate %i[formatter level progname debug info warn error fatal] => :logger
+
+    def initialize logger = Logger.new($stdout), color: Color.new, **attributes
+      @logger = logger
+      @color = color
+      @attributes = attributes
+
+      configure
+      yield logger if block_given?
+    end
+
+    def any(...) = logger.unknown(...)
+
+    def reread = logger.reread
+
+    private
+
+    attr_reader :logger, :color, :attributes
+
+    # rubocop:disable Metrics/AbcSize
+    def configure
+      logger.datetime_format = attributes.fetch :datetime_format, logger.datetime_format
+      logger.level = attributes.fetch :level, default_level
+      logger.progname = attributes.fetch :progname, logger.progname
+      logger.formatter = attributes.fetch :formatter, default_formatter
+    end
+    # rubocop:enable Metrics/AbcSize
+
+    def default_level = logger.class.const_get ENV.fetch("LOG_LEVEL", "INFO")
+
+    def default_formatter
+      ->(severity, _at, _name, message) { "#{color.public_send severity.downcase, message}\n" }
+    end
+  end
+end
---
+++ spec/lib/cogger/client_spec.rb
@@ -0,0 +1,156 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Cogger::Client do
+  using Refinements::StringIOs
+
+  subject(:client) { described_class.new Logger.new(StringIO.new), level: :debug }
+
+  let(:undecorated_output) { Pastel.new.undecorate client.reread }
+
+  describe "#initialize" do
+    let(:formatter) { ->(_severity, _at, _name, message) { "#{message}\n" } }
+
+    it "yields logger when block is given" do
+      client = described_class.new do |logger|
+        logger.formatter = formatter
+        logger.level = :fatal
+        logger.progname = "Test"
+      end
+
+      expect(client).to have_attributes(level: Logger::FATAL, formatter:, progname: "Test")
+    end
+
+    it "uses attributes when given" do
+      client = described_class.new formatter:, level: :fatal, progname: "Test"
+      expect(client).to have_attributes(level: Logger::FATAL, formatter:, progname: "Test")
+    end
+  end
+
+  describe "#formatter" do
+    it "answers default formatter" do
+      expect(client.formatter).to be_a(Proc)
+    end
+
+    it "answers custom formatter" do
+      formatter = proc { "test" }
+      client = described_class.new { |logger| logger.formatter = formatter }
+
+      expect(client.formatter).to eq(formatter)
+    end
+  end
+
+  describe "#level" do
+    it "answers default level" do
+      expect(client.level).to eq(Logger::DEBUG)
+    end
+
+    it "answers custom level" do
+      client = described_class.new { |logger| logger.level = :warn }
+      expect(client.level).to eq(Logger::WARN)
+    end
+  end
+
+  describe "#progname" do
+    it "answers default program name" do
+      expect(client.progname).to be_nil
+    end
+
+    it "answers custom program name" do
+      client = described_class.new { |logger| logger.progname = "Test" }
+      expect(client.progname).to eq("Test")
+    end
+  end
+
+  describe "#debug" do
+    it "answers default color without block" do
+      client.debug "test"
+      expect(undecorated_output).to eq([{foreground: :white, text: "test"}, {text: "\n"}])
+    end
+
+    it "answers default color with block" do
+      client.debug { "test" }
+      expect(undecorated_output).to eq([{foreground: :white, text: "test"}, {text: "\n"}])
+    end
+  end
+
+  describe "#info" do
+    it "answers default color without block" do
+      client.info "test"
+      expect(undecorated_output).to eq([{foreground: :green, text: "test"}, {text: "\n"}])
+    end
+
+    it "answers default color with block" do
+      client.info { "test" }
+      expect(undecorated_output).to eq([{foreground: :green, text: "test"}, {text: "\n"}])
+    end
+  end
+
+  describe "#warn" do
+    it "answers default color without block" do
+      client.warn "test"
+      expect(undecorated_output).to eq([{foreground: :yellow, text: "test"}, {text: "\n"}])
+    end
+
+    it "answers default color with block" do
+      client.warn { "test" }
+      expect(undecorated_output).to eq([{foreground: :yellow, text: "test"}, {text: "\n"}])
+    end
+  end
+
+  describe "#error" do
+    it "answers default color without block" do
+      client.error "test"
+      expect(undecorated_output).to eq([{foreground: :red, text: "test"}, {text: "\n"}])
+    end
+
+    it "answers default color with block" do
+      client.error { "test" }
+      expect(undecorated_output).to eq([{foreground: :red, text: "test"}, {text: "\n"}])
+    end
+  end
+
+  describe "#fatal" do
+    it "answers default color without block" do
+      client.fatal "test"
+
+      expect(undecorated_output).to eq(
+        [{background: :on_red, foreground: :white, style: :bold, text: "test"}, {text: "\n"}]
+      )
+    end
+
+    it "answers default color with block" do
+      client.fatal { "test" }
+
+      expect(undecorated_output).to eq(
+        [{background: :on_red, foreground: :white, style: :bold, text: "test"}, {text: "\n"}]
+      )
+    end
+  end
+
+  describe "#any" do
+    it "answers default color without block" do
+      client.any "test"
+
+      expect(undecorated_output).to eq(
+        [{foreground: :white, style: :bold, text: "test"}, {text: "\n"}]
+      )
+    end
+
+    it "answers default color with block" do
+      client.any { "test" }
+
+      expect(undecorated_output).to eq(
+        [{foreground: :white, style: :bold, text: "test"}, {text: "\n"}]
+      )
+    end
+  end
+
+  describe "#reread" do
+    it "answers what was previously written" do
+      client.info "This is a test."
+      expect(client.reread).to include("This is a test.")
+    end
+  end
+end
