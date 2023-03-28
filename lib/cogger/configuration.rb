c65089691ddf66f5b3c532d985d2cf40e2028512
Brooke Kuhlmann <brooke@alchemists.io>
2023-03-28 15:09:31 -0600

    Added configuration
    Necessary to provide all attributes for configuring a logger upon initialization.
    
    The Reek and RuboCop issues are a concern but are not warranted for this object since the problems stem from the original design of the `Logger` class and is being indirectly surfaced in this design. I will note that `ModuleInitialize` (Reek) is a bug and should be fixed by Reek (I have an open issue on this subject).

---
+++ lib/cogger/configuration.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "logger"
+
+module Cogger
+  # Defines the default configuration for all pipes.
+  Configuration = Data.define(
+    :id,
+    :io,
+    :level,
+    :formatter,
+    :mode,
+    :age,
+    :size,
+    :suffix,
+    :logger
+  ) do
+    def initialize id: Program.call,
+                   io: $stdout,
+                   level: Logger.const_get(ENV.fetch("LOG_LEVEL", "INFO")),
+                   formatter: Formatters::Color.new,
+                   mode: false,
+                   age: 0,
+                   size: 1_048_576,
+                   suffix: "%Y-%m-%d",
+                   logger: Logger
+      super
+    end
+
+    def to_logger
+      logger.new io,
+                 age,
+                 size,
+                 progname: id,
+                 level:,
+                 formatter:,
+                 binmode: mode,
+                 shift_period_suffix: suffix
+    end
+
+    def inspect
+      "#<#{self.class} @id=#{id}, @io=#{io.class}, @level=#{level}, " \
+      "@formatter=#{formatter.class}, " \
+      "@mode=#{mode}, @age=#{age}, @size=#{size}, @suffix=#{suffix.inspect}, " \
+      "@logger=#{logger}>"
+    end
+  end
+end
---
+++ spec/lib/cogger/configuration_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Cogger::Configuration do
+  subject(:configuration) { described_class.new }
+
+  describe "#initialize" do
+    it "answers default configuration" do
+      expect(described_class.new).to have_attributes(
+        id: "rspec",
+        io: $stdout,
+        level: Logger::INFO,
+        formatter: kind_of(Cogger::Formatters::Color),
+        mode: false,
+        age: 0,
+        size: 1_048_576,
+        suffix: "%Y-%m-%d",
+        logger: Logger
+      )
+    end
+  end
+
+  describe "#to_logger" do
+    it "answers logger instance for configuration" do
+      expect(configuration.to_logger).to have_attributes(
+        progname: "rspec",
+        level: Logger::INFO,
+        formatter: kind_of(Cogger::Formatters::Color)
+      )
+    end
+  end
+
+  describe "#inspect" do
+    it "answers attributes" do
+      expect(configuration.inspect).to match(
+        /
+          #<Cogger::Configuration @id=rspec, @io=IO, @level=1,
+          @formatter=Cogger::Formatters::Color,\s@mode=false,
+          \s@age=0,\s@size=1048576,\s@suffix="%Y-%m-%d",\s@logger=Logger>
+        /x
+      )
+    end
+  end
+end
