41bf13640b37b480a543c34d5779f7b0691e26dd
Brooke Kuhlmann <brooke@alchemists.io>
2023-10-18 20:31:19 -0600

    Refactored JSON formatter to not use a template by default
    Necessary to allow the formatter to render JSON in the same positions in which the hash was assembled. Only when a template is supplied can the positions be changed which also provides a slight performance improvement when no templates are used.
    
    Milestone: minor

--- lib/cogger/formatters/json.rb
+++ lib/cogger/formatters/json.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
+require "core"
 require "json"
 
 module Cogger
   module Formatters
     # Formats as JSON output.
     class JSON
-      TEMPLATE = "%<id>s %<severity>s %<at>s %<message>s"
+      TEMPLATE = nil
 
       def initialize template = TEMPLATE,
                      parser: Parsers::Individual.new,
                      sanitizer: Kit::Sanitizer
-        @positions = parser.call(template).last.keys
+        @positions = template ? parser.call(template).last.keys : Core::EMPTY_ARRAY
         @sanitizer = sanitizer
       end
 
       def call(*input)
         attributes = sanitizer.call(*input).tagged_attributes.tap(&:compact!)
+
+        return "#{attributes.to_json}\n" if positions.empty?
+
         "#{attributes.slice(*positions).merge!(attributes.except(*positions)).to_json}\n"
       end
 
