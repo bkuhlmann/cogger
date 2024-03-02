07baf02dc177d594dc6a5c6556acbe5d2a8c3fac
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-02 07:02:10 -0700

    Fixed RuboCop RSpec/DescribedClass issue

--- spec/lib/cogger_spec.rb
+++ spec/lib/cogger_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Cogger do
 
   describe ".new" do
     it "answers hub instance" do
-      expect(described_class.new).to be_a(Cogger::Hub)
+      expect(described_class.new).to be_a(described_class::Hub)
     end
   end
 end
