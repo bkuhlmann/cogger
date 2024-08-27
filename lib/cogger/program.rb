2b06f6c300a54f3147aff99bb2d9348b7c7a1c0c
Brooke Kuhlmann <brooke@alchemists.io>
2024-08-27 09:11:51 -0600

    Fixed program description
    This is more succinct and to the point now.
    
    Milestone: patch

--- lib/cogger/program.rb
+++ lib/cogger/program.rb
@@ -2,7 +2,7 @@
 
 require "pathname"
 
-# Provides a function for computing the default program name based on current file.
+# Computes default program name based on current file name.
 module Cogger
   Program = lambda do |name = $PROGRAM_NAME|
     Pathname(name).then { |path| path.basename(path.extname).to_s }
